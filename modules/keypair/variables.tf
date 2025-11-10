1cb74abbbe67c42ca51eb4b5d5a7719d0a5281d9
Santhosh Raj V <santhoshrajv10@gmail.com>
2025-11-10 15:52:58 +0530

    Update variables.tf

--- modules/keypair/variables.tf
+++ modules/keypair/variables.tf
@@ -1,14 +1,10 @@
-# ============================================
-# Key Pair Module - Outputs
-# File: modules/keypair/outputs.tf
-# ============================================
-
-output "key_pair_name" {
-  description = "Name of the SSH key pair"
-  value       = aws_key_pair.cicd_keypair.key_name
+variable "project_name" {
+  description = "Project name for key pair naming"
+  type        = string
 }
 
-output "key_pair_id" {
-  description = "ID of the SSH key pair"
-  value       = aws_key_pair.cicd_keypair.id
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
